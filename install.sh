2db0af42acd7c21bae6de5d5906d20345f70529b
gabimor-cb <113903717+gabimor-cb@users.noreply.github.com>
2022-12-19 12:35:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-DIR=`dirname $0`
-source "$DIR/docker-prune.sh"
-source "$DIR/docker-stop.sh"
-source "$DIR/pp.sh"
-source "$DIR/ding.sh"
-source "$DIR/mdcd.sh"
-source "$DIR/kill-port.sh"
+# DIR=`dirname $0`
+# source "$DIR/docker-prune.sh"
+# source "$DIR/docker-stop.sh"
+# source "$DIR/pp.sh"
+# source "$DIR/ding.sh"
+# source "$DIR/mdcd.sh"
+# source "$DIR/kill-port.sh"
 
 alias nr='npm run '
